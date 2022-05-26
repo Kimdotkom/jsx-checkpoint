7d7532fc31f1f1a4ff745a9afa646f45a0295f6a
kimdotkom <karimchebbion@gmail.com>
2022-05-26 01:55:57 +0100

    Uploading JSX checkpoint project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+
 import './App.css';
+import image from './image-in-src.jpg';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>JSX Project</h1>
+      <hr></hr>
+
+      <div className='fleex'>
+
+      <div className='elem'>
+      <h3>Image in Public</h3>
+      <img src='./image-in-public.jpg' alt='image1' />
+      </div>
+
+      <hr></hr>
+
+      <div className='elem'>
+      <h3>Image in Scr</h3>
+      <img src={image} alt='image2' />
+      </div>
+
+      </div>
+
+<div style={{border:'solid 1px black', maxWidth:'100vw'}} >
+
+ <h1 className='title red'>Kareem</h1>
+
+ <br />
+
+ <img src={image} alt='' />
+
+ <br />
+
+ <img src='./image-in-public.jpg' alt=''/>
+
+</div>
+
+<video width="320" height="240" controls>
+
+ <source src='myVideo.mp4' type='video/mp4' />
+
+</video>
+
     </div>
   );
 }
